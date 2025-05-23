b48c8cac40597f745dce6ae76331c6c7e9d41b69
Gabriel de Souza da Silva <gabrielsouza65115@gmail.com>
2025-05-23 10:56:40 -0300

    fix: Ajusta links da Navbar

--- src/pages/Index.jsx
+++ src/pages/Index.jsx
@@ -18,24 +18,25 @@ export default function Index() {
 
   return (
     <>
-      <Navbar colorMode={colorMode} />
-
-      <Hero
-        colorMode={colorMode}
-        appDownloadButtons={false}
-        defaultHero={true}
-        influencer={false}
-        mesclado={false}
-      />
-      <Features
-        colorMode={colorMode}
-        defaultFeature={true}
-        button={false}
-        modalWithCards={false}
-        paragraphs={false}
-        sixCards={false}
-        paragraphsModal={false}
-      />
+      <div>
+        <Navbar colorMode={colorMode} />
+        <Hero
+          colorMode={colorMode}
+          appDownloadButtons={false}
+          defaultHero={true}
+          influencer={false}
+          mesclado={false}
+        />
+        <Features
+          colorMode={colorMode}
+          defaultFeature={true}
+          button={false}
+          modalWithCards={false}
+          paragraphs={false}
+          sixCards={false}
+          paragraphsModal={false}
+        />
+      </div>
       <About modal={true} showGallery={false} colorMode={colorMode} />
       <AboutInstagram
         colorMode={colorMode}
