6a1cdff7c6abc77e4007d7511ff7a7ba5e12c06f
Gabriel de Souza da Silva <gabrielsouza65115@gmail.com>
2025-05-26 10:26:04 -0300

    fix: Aumenta a logo nas telas

--- src/components/sections/NavbarSocial.jsx
+++ src/components/sections/NavbarSocial.jsx
@@ -102,7 +102,7 @@ export default function NavbarSocial({ colorMode }) {
         <Navbar>
           <ScrollLink
             to="home"
-            className="cursor-pointer w-[50%] phone2:w-[50%] phone3:w-[45%] tablet1:w-[30%] tablet2:w-[30%] desktop1:w-[20%] desktop2:max-w-[190px]"
+            className="cursor-pointer w-[60%] phone2:w-[65%] phone3:w-[70%] tablet1:w-[40%] tablet2:w-[40%] desktop1:w-[40%] desktop2:w-[350px]"
             spy={true}
             smooth={true}
             duration={500}
