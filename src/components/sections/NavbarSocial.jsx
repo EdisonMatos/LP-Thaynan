e7ac2efc61c0842630e661b277182e66a1ab32b8
Edison Matos <edison.matos@live.com>
2025-05-27 14:47:40 -0300

    feat: Corrige tamanho da logo após scroll

--- src/components/sections/NavbarSocial.jsx
+++ src/components/sections/NavbarSocial.jsx
@@ -80,9 +80,7 @@ export default function NavbarSocial({ colorMode }) {
 
   const getNavbarClasses = () => {
     if (colorMode === "light") {
-      return scrolling
-        ? "bg-white shadow-md"
-        : "desktop1:bg-white/20";
+      return scrolling ? "bg-white shadow-md" : "desktop1:bg-white/20";
     }
     if (colorMode === "dark") {
       return scrolling
@@ -97,8 +95,9 @@ export default function NavbarSocial({ colorMode }) {
 
   return (
     <div className="w-full">
-
-      <div className={`fixed z-20 w-full transition-colors duration-1000 ${getNavbarClasses()}`}>
+      <div
+        className={`fixed z-20 w-full transition-colors duration-1000 ${getNavbarClasses()}`}
+      >
         <Navbar>
           <ScrollLink
             to="home"
@@ -114,7 +113,7 @@ export default function NavbarSocial({ colorMode }) {
               alt={content.texts.navbar.logo.alt}
               className={`bg-transparent ${
                 scrolling
-                  ? "w-[90%] tablet1:w-[80%] tablet2:w-[70%] desktop1:w-[90%] desktop2:w-[80%]"
+                  ? "w-[90%] tablet1:w-[80%] tablet2:w-[70%] desktop1:w-[50%]"
                   : "my-[20px] w-full"
               } transition-all duration-1000 tablet3:mb-0`}
             />
@@ -140,4 +139,3 @@ export default function NavbarSocial({ colorMode }) {
     </div>
   );
 }
-
