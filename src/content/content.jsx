364d58c1f9f9f2698226efa69f5128e8d5d0429a
Gabriel de Souza da Silva <gabrielsouza65115@gmail.com>
2025-05-30 06:56:52 -0300

    fix: Copy Footer

--- src/content/content.jsx
+++ src/content/content.jsx
@@ -77,7 +77,8 @@ export const infos = {
     <div>
       <p>Endereço:</p>
       <p>Rua 222, n. 258, sala 4,</p>
-      <p>Meia Praia Itapema - SC,</p>
+      <p>Meia Praia,</p>
+      <p>Itapema - SC,</p>
       <p>CEP 88220-000.</p>
     </div>
   ),
